4de0cc5d21cbfc09f6756ef9a54ad4449c105b45
richardc <richardc@c56e198b-7db7-0310-b3ab-8e406f684e33>
2006-12-16 12:49:02 +0000

    add a new format to identify
    git-svn-id: https://svn.unixbeard.net/richardc/Warezomatic@4360 c56e198b-7db7-0310-b3ab-8e406f684e33

--- lib/Warez/Identify.pm
+++ lib/Warez/Identify.pm
@@ -9,13 +9,13 @@ sub identify {
     my $path = shift;
     my $file = basename $path;
 
-    my ($show, $ep, $extra) = $file =~ m{^(.*?)(\d{3,}|s\d+e\d+|\d+x\d+)(.*)}i
+    my ($show, $ep, $extra) = $file =~ m{^(.*?)(\d{3,}|s\d+\s*ep?\d+|\d+x\d+)(.*)}i
       or return;
     $show =~ s{[_\.]}{ }g; # dots or underscores for spaces is common
     $show =~ s{\s*$}{};    # but also as seperators
     $show =~ s{\s+-$}{};   # trailing hypens are probably delimiters
 
-    $ep =~ m{^(\d+?)(\d\d)$} or $ep =~ m{^s?(\d+)[ex](\d+)}i
+    $ep =~ m{^(\d+?)(\d\d)$} or $ep =~ m{^s?(\d+)\s*[ex]p?(\d+)}i
       or die "couldn't match $ep as episode";
     my ($season, $episode) = (0+$1, 0+$2);
     return {
